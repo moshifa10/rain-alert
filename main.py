1edb0ec34195810e572a3c2a77f6d73f5e9f87f5
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-02-15 03:24:19 +0200

    request is working and hidden my passwords

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-
-
+import requests
+import dotenv
+import os
 # I will implement rain alert using sms and openweather api
+
+ENDPOINT = "https://api.openweathermap.org/data/2.5/weather"
+dotenv.load_dotenv()
+
+api_key = os.getenv(key="API_KEY")
+lon = os.getenv(key="LON")
+lat = os.getenv(key="LAT")
+
+
+# parameter = {
+#     "lat": lat,
+#     "lon": lon,
+#     "appid": api_key
+# }
+
+response = requests.get(url=f"{ENDPOINT}?lat={lat}&lon={lon}&appid={api_key}")
+
+response.raise_for_status()
+
+data = None
+if response.status_code == 200:
+    data = response.json()
+
+print(data)
