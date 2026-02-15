07da35c1a19e37d178295979ca13a8b69297023a
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-02-15 20:48:05 +0200

    now I implement api using twilio

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import dotenv
 import os
+import pprint
 # I will implement rain alert using sms and openweather api
 
 ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
@@ -14,15 +15,20 @@ lat = os.getenv(key="LAT")
 parameter = {
     "lat": lat,
     "lon": lon,
-    "appid": api_key
+    "appid": api_key,
+    "cnt": 4
 }
 
 response = requests.get(url=ENDPOINT, params=parameter)
 
 response.raise_for_status()
 
-data = None
-if response.status_code == 200:
-    data = response.json()
+data = response.json()
 
-print(data)
+# get data 
+pprint.pprint(data)
+for weather in data["list"]:
+    code = weather["weather"][0]["id"]
+
+    if code <700:
+        print(f"Bring up your umbrella {code}")
